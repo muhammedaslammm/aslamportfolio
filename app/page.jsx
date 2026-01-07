75c59e812f87c67446a3513dc8d9107d33b0697f
Muhammed Aslam <muhammedbinramli@gmail.com>
2026-01-07 17:38:39 +0530

    (Mobile):new layout

--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="w-[80%] mx-auto py-8 h-screen flex flex-col text-[1.1rem]">
+    <main className="w-[85%] xl:w-[80%] mx-auto py-4 xl:py-8 h-screen flex flex-col text-[.8rem] xl:text-[1.1rem]">
       <header className="flex justify-between items-center ">
         <div className="">Muhammed Aslam</div>
         <Link href="/" className="hover:text-neutral-400 transition-colors">
           About Me
         </Link>
       </header>
-      <section className="flex-1 text-[2rem] pt-[4rem]">
-        Full Stack developer with experience building static websites <br /> and
-        full stack web applications for real clients.
+      <section className="flex-1 text-[2rem]">
+        <div className="pt-[4rem]">
+          Full stack developer with experience building static websites <br />{" "}
+          and full stack web applications for real clients.
+        </div>
       </section>
       <footer className="flex justify-between items-center">
         <p className="text-neutral-400">Muhammed Aslam. All rights reserved.</p>
